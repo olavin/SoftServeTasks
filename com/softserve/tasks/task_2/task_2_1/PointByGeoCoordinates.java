3ed32101b1941ffa5ebfe9ad695b12e109cccd4b
DrBAXA <drbaxa@hotmail.com>
2014-10-18 01:39:44 +0300

    Adding files of task_2_1 to repositiry

---
+++ com/softserve/tasks/task_2/task_2_1/PointByGeoCoordinates.java
@@ -0,0 +1,48 @@
+package com.softserve.tasks.task_2.task_2_1;
+
+import com.softserve.tasks.task_2.task_2_1.IPoint;
+
+/**
+ * Created by BAXA on 15.10.2014.
+ */
+public class PointByGeoCoordinates implements IPoint {
+    private double latitude;
+    private double longitude;
+
+    @Override
+    public String toString() {
+        return "PointByGeoCoordinates{" +
+                  latitude + " " +
+                  longitude +
+                '}';
+    }
+
+    public PointByGeoCoordinates(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitudeOnRad() {
+        return latitude/180*Math.PI;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitudeOnRad() {
+        return longitude/180*Math.PI;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
